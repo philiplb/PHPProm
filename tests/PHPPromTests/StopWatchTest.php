dff3294b22e00a297655fe1ad1c9271ca4834fca
Philip Lehmann-Boehm <philip@philiplb.de>
2016-11-07 23:18:09 +0100

    testing the StopWatch

---
+++ tests/PHPPromTests/StopWatchTest.php
@@ -0,0 +1,37 @@
+<?php
+
+/*
+ * This file is part of the PHPProm package.
+ *
+ * (c) Philip Lehmann-Böhm <philip@philiplb.de>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace PHPPromTests;
+
+use PHPUnit\Framework\TestCase;
+use Eloquent\Phony\Phpunit\Phony;
+use PHPProm\StopWatch;
+
+class StopWatchTest extends TestCase{
+
+    public function testStartStop() {
+        $storageHandle = Phony::mock('\\PHPProm\\Storage\\AbstractStorage');
+        $storageMock   = $storageHandle->get();
+
+        $stopWatch = new StopWatch($storageMock);
+        $stopWatch->start();
+        sleep(1);
+        $stopWatch->stop('prefix', 'key');
+
+        $storageHandle->storeMeasurement->once()->called();
+        $call      = $storageHandle->storeMeasurement->firstCall();
+        $arguments = $call->arguments();
+        $this->assertSame('prefix', $arguments->get(0));
+        $this->assertSame('key', $arguments->get(1));
+        $this->assertTrue(abs($arguments->get(2) - 1.0) < 0.01);
+    }
+
+}

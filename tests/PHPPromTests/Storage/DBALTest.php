59739b3025eb75008cd84c2e57ab297e7cbcea3e
Philip Lehmann-Boehm <philip@philiplb.de>
2016-11-08 22:37:36 +0100

    testing the DBAL storage

---
+++ tests/PHPPromTests/Storage/DBALTest.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace PHPPromTests\Storage;
+
+use PHPProm\Storage\DBAL;
+
+class DBALTest extends AbstractStorageTest {
+
+    protected $database;
+
+    protected function setUp() {
+        $config = new \Doctrine\DBAL\Configuration();
+        $connectionParams = array(
+            'url' => 'mysql://root:@localhost/phppromtest',
+        );
+        $this->database = \Doctrine\DBAL\DriverManager::getConnection($connectionParams, $config);
+
+        $this->storage = new DBAL($this->database);
+
+        $sql = 'DROP TABLE IF EXISTS `phpprom`';
+        $this->database->executeUpdate($sql);
+        $sql = 'CREATE TABLE `phpprom` (
+              `key` varchar(255) NOT NULL,
+              `value` double NOT NULL,
+              PRIMARY KEY (`key`)
+            ) ENGINE=InnoDB DEFAULT CHARSET=utf8';
+        $this->database->executeUpdate($sql);
+    }
+
+    protected function getRawKey($key) {
+        $sql = 'SELECT `value` FROM phpprom WHERE `key` = ?';
+        $result = $this->database->fetchAssoc($sql, [substr($key, strlen('PHPProm:'))]);
+        return (int)$result['value'];
+    }
+
+}

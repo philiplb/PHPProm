606731cc63125485711b77db587ff01a20d930ca
Philip Lehmann-Boehm <philip@philiplb.de>
2016-11-11 12:49:08 +0100

    added missing license hints

--- tests/PHPPromTests/Storage/AbstractStorageTest.php
+++ tests/PHPPromTests/Storage/AbstractStorageTest.php
@@ -1,5 +1,14 @@
 <?php
 
+/*
+ * This file is part of the PHPProm package.
+ *
+ * (c) Philip Lehmann-Böhm <philip@philiplb.de>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace PHPPromTests\Storage;
 
 abstract class AbstractStorageTest extends \PHPUnit_Framework_TestCase {
--- tests/PHPPromTests/Storage/DBALTest.php
+++ tests/PHPPromTests/Storage/DBALTest.php
@@ -1,5 +1,14 @@
 <?php
 
+/*
+ * This file is part of the PHPProm package.
+ *
+ * (c) Philip Lehmann-Böhm <philip@philiplb.de>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace PHPPromTests\Storage;
 
 use PHPProm\Storage\DBAL;
